a20b50251636191b36eac4b503f9970eac5defee
Felix0102 <felix0102@qq.com>
2022-11-23 08:50:05 +0800

    Add files via upload

--- myemail.py
+++ myemail.py
@@ -8,7 +8,8 @@ from email.header import Header
 # 第三方 SMTP 服务
 mail_host="smtp.163.com"  #设置服务器
 mail_user="sys__warning"    #用户名
-mail_pass="Syswarning__123"   #口令 
+#mail_pass="Syswarning__123"   #口令 
+mail_pass="PWCDHVIAWQNLXBJI"   #口令 
 sender = 'sys__warning@163.com'
 receivers = ['3040207@qq.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
    
