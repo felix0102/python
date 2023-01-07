3b9e8266b1b5afd895c47f98a15e624ddf82ea37
Felix0102 <felix0102@qq.com>
2023-01-07 11:22:07 +0800

    Add files via upload

--- oggMonitor.py
+++ oggMonitor.py
@@ -5,6 +5,7 @@
 import myemail
 import requests
 from requests.auth import HTTPBasicAuth
+from common.logger_handler import logger
 # 现在可以调用模块里包含的函数了
 # myemail.sendemail("title","content")
 
@@ -21,8 +22,8 @@ def getServiceStatus(url):
 
 monitored_URLS = {
     "EXTP6_1_URL": "http://10.2.137.14:9012/services/v2/extracts/EXTP6_1/info/status",
-    "EXP6_2_URL": "http://10.2.137.19:9012/services/v2/extracts/EXP6_2/info/status",
-    "EXP6_3_URL": "http://10.2.137.35:9012/services/v2/extracts/EXP6_3/info/status",
+    "EXTP6_2_URL": "http://10.2.137.19:9012/services/v2/extracts/EXP6_2/info/status",
+    "EXTP6_3_URL": "http://10.2.137.35:9012/services/v2/extracts/EXP6_3/info/status",
     "RECP6_1_URL":"http://10.2.60.38:9014/services/v2/targets/RECP6_1",
     "RECP6_2_URL":"http://10.2.60.38:9014/services/v2/targets/RECP6_2",
     "RECP6_3_URL":"http://10.2.60.38:9014/services/v2/targets/RECP6_3",
@@ -31,12 +32,16 @@ monitored_URLS = {
     "REPP63_URL": "http://10.2.60.38:9012/services/v2/replicats/REPP63/info/status",
 }
 
+content =""
+status =""
 for k in monitored_URLS.keys():
     theURL = monitored_URLS.get(k)
     status = getServiceStatus(theURL)
     if (status == 'running'):
-        print("running")
+        logger.debug("running")
     else:
-        print("not running")
-        content = k + ": not running :"+theURL
-        myemail.sendemail("OGG service ERROR!!!", content)
+        logger.debug("not running")
+        content = "ERROR not running : " +k +" : "+theURL  +"\r\n" +content 
+
+if (content !=""):
+    myemail.sendemail("OGG service ERROR!!!", content)
